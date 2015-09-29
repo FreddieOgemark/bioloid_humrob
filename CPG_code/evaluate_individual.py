8a1833e28d87d2c0f22bea402776ad1b20a0adf0
Gustav Olsson <gustav128@gmail.com>
2015-09-29 14:06:31 +0200

    report distance travelled

--- CPG_code/evaluate_individual.py
+++ CPG_code/evaluate_individual.py
@@ -10,11 +10,11 @@ except:
     print ('')
 
 import time
+import math
 
 def evaluate_individual():
     print('Evaluating individual...')
 
-def test():
     vrep.simxFinish(-1)
     clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 1000, 5)
     print('Connected to VREP.')
@@ -22,28 +22,45 @@ def test():
     if clientID != -1:
         # Get joint handles
         jointIndices = [15,13,9,12,14,16,1,2]
-        handles = []
+        jointHandles = []
 
         for i in jointIndices:
             _, h = vrep.simxGetObjectHandle(clientID, 'Joint' + str(i), vrep.simx_opmode_oneshot_wait)
-            handles.append(h)
+            jointHandles.append(h)
         print('Joint handles received.')
+
+        # Get bioloid handle
+        _, torsoHandle = vrep.simxGetObjectHandle(clientID, 'Bioloid', vrep.simx_opmode_oneshot_wait)
 
         # Start simulation
         vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
         vrep.simxSynchronous(clientID, True)
         print('Sim started.')
 
-        for i in range(0,100):
+        totalDistance = 0
+        _, lastPos = vrep.simxGetObjectPosition(clientID, torsoHandle, -1, vrep.simx_opmode_oneshot_wait)
+
+        for iteration in range(0,100):
             # CPG network step
             output = [-1.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0]
 
             # Set joint angles
-            for i in range(0,len(handles)):
-                vrep.simxSetJointTargetPosition(clientID, handles[i], output[i], vrep.simx_opmode_oneshot)
+            for i in range(0,len(jointHandles)):
+                vrep.simxSetJointTargetPosition(clientID, jointHandles[i], output[i], vrep.simx_opmode_oneshot)
+
+            # Measure how far the robot moved every 10th (for now) time step
+            if iteration % 10 == 0:
+                _, currPos = vrep.simxGetObjectPosition(clientID, torsoHandle, -1, vrep.simx_opmode_oneshot_wait)
+                delta = [ currPos[0] - lastPos[0], currPos[1] - lastPos[1] ]
+                distance = math.sqrt(delta[0]*delta[0] + delta[1]*delta[1])
+                lastPos = currPos
+                totalDistance += distance
 
             # VREP simulation step
             vrep.simxSynchronousTrigger(clientID)
+
+        # Measure movement
+        print('Total distance travelled (in any direction): ' + str(totalDistance))
 
         # Stop simulation
         vrep.simxSynchronous(clientID, False)
@@ -52,5 +69,7 @@ def test():
         vrep.simxFinish(clientID)
         print('Disconnected from VREP.')
 
-# uncomment the following line to test
-test()
+    print('Done.')
+    return totalDistance
+
+evaluate_individual()
