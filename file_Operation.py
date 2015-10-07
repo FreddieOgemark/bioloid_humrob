184bd64a214b0d5423478b6058ae66458942ce23
Alireza Tashvir <alireza.tashvir@gmail.com>
2015-10-07 15:38:54 +0200

    Make it more functional

--- file_Operation.py
+++ file_Operation.py
@@ -1,25 +1,51 @@
+import os
+
 class fileOperations:
 
-    def __init__(self, fileName):
-        
+    def __init__(self, folderName):
+        fileName = self.getLastGeneratedFile(folderName)
+        #print fileName
         self.lines = []
         self.parameterValues = []
         with open(fileName) as f:
             self.lines = self.lines + f.readlines()
 
 
-    def showContent(self):
+    def getContent(self):
         
         for i in range(len(self.lines)):
             values = self.lines[i].replace("\n","").split(',')
             for j in range(len(values)):
                 if values[j].replace(" ","") != "":
-                    print(values[j])
+                    #print(values[j])
                     self.parameterValues.append(float(values[j]))
 
         return self.parameterValues
 
+    def getLastGeneratedFile(self, folderName):
+        txtFilesInDir = []
+        if os.path.isdir(folderName):
+            for root, dirs, files in os.walk(folderName):
+                for name in files:
+                    if name.endswith(("csv")):
+                        #print os.path.join(root, name)
+                        txtFilesInDir.append(name)
+                        #print name
 
-#f = fileOperations('2015-10-02_13-41-35_bestGenome.csv')
+        maxDate = ""
+        for fName in txtFilesInDir:
+            #print fName[0:19]
+            if fName[0:19] >= maxDate:
+                #print "True"
+                maxDate = fName[0:19]
+                lastFile = fName
+                
+        #print lastFile
+        return lastFile
+                        
+
+#f = fileOperations("D:\Alireza_UniDocuments\Chalmers University\Semester 3\Humanoid Robotics\Git\TIF160_Bioloid")
 #content = f.showContent()
 #print(content)
+
+#f.getContent()
