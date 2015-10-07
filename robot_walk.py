ce12a543b66147a8fb000b38a0068b1eabfcc865
Gustav Olsson <gustav128@gmail.com>
2015-10-07 14:22:27 +0200

    base pose at start

--- robot_walk.py
+++ robot_walk.py
@@ -14,7 +14,28 @@ class BioloidControl:
         for i in range(1,19):
             servo = Robotis_Servo(self.dyn,i)
             self.servos.append(servo)
-            self.servoAngle.append[0]
+            self.servoAngle.append(0)
+
+    def moveToBasePose(self, delay):
+        self.setJointPosition(0, 0.0)
+        self.setJointPosition(1, 0.0)
+        self.setJointPosition(2, 0.0)
+        self.setJointPosition(3, 0.0)
+        self.setJointPosition(4, 0.0)
+        self.setJointPosition(5, 0.0)
+        self.setJointPosition(6, -45.0*math.pi/180.0)
+        self.setJointPosition(7, 45.0*math.pi/180.0)
+        self.setJointPosition(8, 0.0)
+        self.setJointPosition(9, 0.0)
+        self.setJointPosition(10, 0.0)
+        self.setJointPosition(11, 0.0)
+        self.setJointPosition(12, 0.0)
+        self.setJointPosition(13, 0.0)
+        self.setJointPosition(14, 0.0)
+        self.setJointPosition(15, 0.0)
+        self.setJointPosition(16, 0.0)
+        self.setJointPosition(17, 0.0)
+        self.move(delay)
 
     def setJointPosition(self, idx, position):
         self.servoAngle[idx] = position
@@ -35,6 +56,9 @@ def evaluate_individual(genomeFileName):
     if bioloid.dev_name:
         deltaTime = 0.01
 
+        # Wait 6 seconds for base pose to stabilize
+        bioloid.moveToBasePose(6.0)
+
         # Shoulders should point downwards by default!
         # Join [15,13,9,12,14,16,1,2]
         jointIndices = [14,12,8,11,13,15,0,1]
@@ -45,16 +69,13 @@ def evaluate_individual(genomeFileName):
         shoulderLeftIdx = 3 # Joint 4
 
         # Set initial joint angles
-        for i in range(0,len(jointHandles)):
+        for i in range(0,len(jointIndices)):
             bioloid.setJointPosition(jointIndices[i], jointOffsets[i])
 
         # Set joints that are not considered by the optimizaton
         shoulderAngle = math.radians(80.0)
         bioloid.setJointPosition(shoulderRightIdx, shoulderAngle)
         bioloid.setJointPosition(shoulderLeftIdx, shoulderAngle)
-
-        # Wait 5 seconds for the base pose to stabilize
-        bioloid.move(5.0)
 
         # Evaluate for a number of integration steps
         bn = cpg.bioloid_network.BioloidNetwork(genome, deltaTime)
@@ -63,13 +84,12 @@ def evaluate_individual(genomeFileName):
             jointAngles = bn.get_output()
 
             # Set joint angles
-            for i in range(0,len(jointHandles)):
+            for i in range(0,len(jointIndices)):
                 bioloid.setJointPosition(jointIndices[i], jointOffsets[i] + jointAngles[i])
 
             # move robot
             bioloid.move(deltaTime)
-
-        # Stop robot (TODO : unnecessary here?)
+            
     print('Done.')
 
 evaluate_individual('genomeData/2015-10-02_15-51-12_bestGenome.csv')
