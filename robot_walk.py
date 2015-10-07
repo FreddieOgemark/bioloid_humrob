4e4eb1b0dcad6d2b2293ff9697ffe368de74838b
Gustav Olsson <gustav128@gmail.com>
2015-10-07 14:01:48 +0200

    small fixes to robot_walk.py

--- robot_walk.py
+++ robot_walk.py
@@ -1,76 +1,11 @@
-#from lib_robotis import *
-#from usbscan import *
-
 import time
 import math
 import cpg.bioloid_network
 from robot_control.lib_robotis import *
 from robot_control.usbscan import *
-import file_Operation
+import file_operation
 
-
-def evaluate_individual(genomeFileName):
-    f = fileOperations(genomeFileName)
-    genome = f.showContent()
-    bioloid = bioloid_control.BioloidControl();
-
-    if bioloid.devName:
-        deltaTime = 0.01
-
-        # Shoulders should point downwards by default!
-        jointIndices = [14,12,8,11,13,15,0,1]
-        jointOffsets = [0.0,0.0,0.0,0.0,0.0,0.0,-math.radians(90.0),-math.radians(90.0)]
-
-        # Get handles
-        #_, pivotHandle = vrep.simxGetObjectHandle(clientID, 'Pivot', vrep.simx_opmode_oneshot_wait)
-        shoulderRightIdx = 2 #joint3
-        shoulderLeftIdx = 3 #joint4
-
-        print('Object handles received.')
-
-        # Get bioloid handle
-        #_, torsoHandle = vrep.simxGetObjectHandle(clientID, 'Bioloid', vrep.simx_opmode_oneshot_wait)
-
-        # Start simulation
-        #vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
-        #vrep.simxSynchronous(clientID, True)
-        #print('Sim started.')
-
-        # Speed up simulation
-        #vrep.simxSetIntegerParameter(clientID, vrep.sim_intparam_speedmodifier, 10, vrep.simx_opmode_oneshot_wait)
-
-        # Set initial joint angles
-        for i in range(0,len(jointHandles)):
-            bioloid.setJointPosition(jointIndices[i], jointOffsets[i])
-
-        # Set joints that are not considered by the optimizaton
-        shoulderAngle = math.radians(80.0)
-        bioloid.setJointPosition(shoulderRightIdx, shoulderAngle)
-        bioloid.setJointPosition(shoulderLeftIdx, shoulderAngle)
-
-        # Evaluate for a number of integration steps
-        position = 0
-        maxPosition = 0
-        #lastPos = vrep.simxGetObjectPosition(clientID, torsoHandle, -1, vrep.simx_opmode_oneshot_wait)
-
-        bn = cpg.bioloid_network.BioloidNetwork(genome, deltaTime)
-        for iteration in range(0,200):
-            # CPG network step
-            jointAngles = bn.get_output()
-
-            # Set joint angles
-            for i in range(0,len(jointHandles)):
-                bioloid.setJointPosition(jointIndices[i], jointOffsets[i] + jointAngles[i])
-
-            # move robot
-            bioloid.move()
-
-        # Stop robot (TODO : unnecessary here?)
-
-    print('Done.')
-    
-
-class Bioloid:
+class BioloidControl:
     def __init__(self, usbPort=None):
         self.dev_name = usbPort if usbPort else scan_for_usb()
         self.dyn = USB2Dynamixel_Device(self.dev_name)
@@ -84,11 +19,57 @@ class Bioloid:
     def setJointPosition(self, idx, position):
         self.servoAngle[idx] = position
 
-    def move(self):
+    def move(self, postUpdateDelay):
         vel = math.radians(100.0)
-        # Base-pose:
+
         for i in range(len(self.servos)):
-            servos[i].move_angle(self.servoAngle[i], vel, True) # True here means block call until movement done
+            servos[i].move_angle(self.servoAngle[i], vel, False) # True here means block call until movement done
 
+        pause(postUpdateDelay)
 
-#evaluate_individual('2015-10-02_13-41-35_bestGenome.csv')
+def evaluate_individual(genomeFileName):
+    f = file_operation.FileOperations(genomeFileName)
+    genome = f.showContent()
+    bioloid = BioloidControl();
+
+    if bioloid.dev_name:
+        deltaTime = 0.01
+
+        # Shoulders should point downwards by default!
+        # Join [15,13,9,12,14,16,1,2]
+        jointIndices = [14,12,8,11,13,15,0,1]
+        jointOffsets = [0.0,0.0,0.0,0.0,0.0,0.0,-math.radians(90.0),-math.radians(90.0)]
+
+        # Get handles
+        shoulderRightIdx = 2 # Joint 3
+        shoulderLeftIdx = 3 # Joint 4
+
+        # Set initial joint angles
+        for i in range(0,len(jointHandles)):
+            bioloid.setJointPosition(jointIndices[i], jointOffsets[i])
+
+        # Set joints that are not considered by the optimizaton
+        shoulderAngle = math.radians(80.0)
+        bioloid.setJointPosition(shoulderRightIdx, shoulderAngle)
+        bioloid.setJointPosition(shoulderLeftIdx, shoulderAngle)
+
+        # Wait 5 seconds for the base pose to stabilize
+        bioloid.move(5.0)
+
+        # Evaluate for a number of integration steps
+        bn = cpg.bioloid_network.BioloidNetwork(genome, deltaTime)
+        for iteration in range(0,200):
+            # CPG network step
+            jointAngles = bn.get_output()
+
+            # Set joint angles
+            for i in range(0,len(jointHandles)):
+                bioloid.setJointPosition(jointIndices[i], jointOffsets[i] + jointAngles[i])
+
+            # move robot
+            bioloid.move(deltaTime)
+
+        # Stop robot (TODO : unnecessary here?)
+    print('Done.')
+
+evaluate_individual('genomeData/2015-10-02_15-51-12_bestGenome.csv')
